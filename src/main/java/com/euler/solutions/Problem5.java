52160a441cf3641a69e1ddd10448ef2fa0cb4b33
Jon Eland <jonathan.eland@gmail.com>
2014-02-06 22:52:56 +0000

    Adding skeleton for Problem 5

---
+++ src/main/java/com/euler/solutions/Problem5.java
@@ -0,0 +1,7 @@
+package com.euler.solutions;
+
+public class Problem5 {
+	public int calculateSmallestNumberEvenlyDivisibleByAllNumbersBetween(int lowerBoundary, int upperBoundary) {
+		return 2520;
+	}
+}
---
+++ src/test/java/com/euler/solutions/Problem5Test.java
@@ -0,0 +1,17 @@
+package com.euler.solutions;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class Problem5Test {
+	private Problem5 problem5 = new Problem5();
+
+	@Test public void
+	calculates_smallest_number_evenly_divisible_by_all_numbers_between_1_and_10_to_be_2520() {
+		int result = problem5.calculateSmallestNumberEvenlyDivisibleByAllNumbersBetween(1, 10);
+
+		assertThat(result, is(2520));
+	}
+}
