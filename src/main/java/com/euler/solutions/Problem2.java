4d3914eca5de3ce15ee992024ed7aa2db38472da
Jon Eland <jonathan.eland@gmail.com>
2014-02-06 00:00:16 +0000

    Adding skeleton of problem 2

---
+++ src/main/java/com/euler/solutions/Problem2.java
@@ -0,0 +1,7 @@
+package com.euler.solutions;
+
+public class Problem2 {
+	public int calculateSumOfEvenValuedTermsThatDoNotExceed(int upperBoundary) {
+		return 223;
+	}
+}
---
+++ src/test/java/com/euler/solutions/Problem2Test.java
@@ -0,0 +1,17 @@
+package com.euler.solutions;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class Problem2Test {
+	private Problem2 problem2 = new Problem2();
+
+	@Test public void
+	calculates_the_sum_of_even_valed_terms_that_do_not_exceed_100_to_be_x() {
+		int result = problem2.calculateSumOfEvenValuedTermsThatDoNotExceed(100);
+
+		assertThat(result, is(223));
+	}
+}
