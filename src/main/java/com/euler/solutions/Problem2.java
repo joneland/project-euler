10b7fbde929b55a71175d1333b1f6e52b376d245
Jon Eland <jonathan.eland@gmail.com>
2014-02-06 12:07:07 +0000

    Adding solution for Problem 2

--- src/main/java/com/euler/solutions/Problem2.java
+++ src/main/java/com/euler/solutions/Problem2.java
@@ -1,7 +1,57 @@
 package com.euler.solutions;
 
+import static com.google.common.math.IntMath.checkedAdd;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
 	public int calculateSumOfEvenValuedTermsThatDoNotExceed(int upperBoundary) {
-		return 223;
+		List<Integer> fibonacciSequence = initialiseFibonacciSequence();
+
+		while (true) {
+			int nextTerm = calculateNextTermIn(fibonacciSequence);
+
+			if (nextTerm < upperBoundary) {
+				fibonacciSequence.add(nextTerm);
+			}
+
+			if (nextTerm >= upperBoundary) {
+				break;
+			}
+		}
+
+		return sumOfEvenValuedTermsIn(fibonacciSequence);
+	}
+
+	private int calculateNextTermIn(List<Integer> fibonacciSequence) {
+		int secondToLastTermInSequence = fibonacciSequence.get(secondToLastTermIn(fibonacciSequence));
+		int lastTermInSequence = fibonacciSequence.get(lastTermIn(fibonacciSequence));
+		return checkedAdd(secondToLastTermInSequence, lastTermInSequence);
+	}
+
+	private int lastTermIn(List<Integer> fibonacciSequence) {
+		return fibonacciSequence.size() - 1;
+	}
+
+	private int secondToLastTermIn(List<Integer> fibonacciSequence) {
+		return fibonacciSequence.size() - 2;
+	}
+
+	private List<Integer> initialiseFibonacciSequence() {
+		List<Integer> fibonacciSequence = new ArrayList<Integer>();
+		fibonacciSequence.add(1);
+		fibonacciSequence.add(2);
+		return fibonacciSequence;
+	}
+
+	private int sumOfEvenValuedTermsIn(List<Integer> fibonacciSequence) {
+		int result = 0;
+		for (int term : fibonacciSequence) {
+			if (term % 2 == 0) {
+				result = checkedAdd(result, term);
+			}
+		}
+		return result;
 	}
 }
--- src/test/java/com/euler/solutions/Problem2Test.java
+++ src/test/java/com/euler/solutions/Problem2Test.java
@@ -9,9 +9,16 @@ public class Problem2Test {
 	private Problem2 problem2 = new Problem2();
 
 	@Test public void
-	calculates_the_sum_of_even_valed_terms_that_do_not_exceed_100_to_be_x() {
+	calculates_the_sum_of_even_valed_terms_that_do_not_exceed_100_to_be_44() {
 		int result = problem2.calculateSumOfEvenValuedTermsThatDoNotExceed(100);
 
-		assertThat(result, is(223));
+		assertThat(result, is(44));
+	}
+
+	@Test public void
+	calculates_the_sum_of_even_valed_terms_that_do_not_exceed_4000000_to_be_44() {
+		int result = problem2.calculateSumOfEvenValuedTermsThatDoNotExceed(4000000);
+
+		assertThat(result, is(4613732));
 	}
 }
