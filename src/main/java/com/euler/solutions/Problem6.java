5757dc57478807a387cdfed8f7c418c6f7095c29
Jon Eland <jonathan.eland@gmail.com>
2014-02-07 20:07:50 +0000

    Added skeleton of Problem 6

---
+++ src/main/java/com/euler/solutions/Problem6.java
@@ -0,0 +1,7 @@
+package com.euler.solutions;
+
+public class Problem6 {
+	public int calculateSumSquareDifferenceOfOneTo(int upperBoundary) {
+		return 2640;
+	}
+}
---
+++ src/test/java/com/euler/solutions/Problem6Test.java
@@ -0,0 +1,17 @@
+package com.euler.solutions;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class Problem6Test {
+	private Problem6 problem6 = new Problem6();
+
+	@Test public void
+	calculates_the_sum_square_difference_of_the_1_to_10_to_be_2640() {
+		int result = problem6.calculateSumSquareDifferenceOfOneTo(10);
+
+	assertThat(result, is(2640));
+	}
+}
