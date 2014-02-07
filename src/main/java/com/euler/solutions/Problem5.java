37b9c89497e22a29312b81da5827f26c5d9aec82
Jon Eland <jonathan.eland@gmail.com>
2014-02-07 19:39:49 +0000

    Added solution for Problem 5

--- src/main/java/com/euler/solutions/Problem5.java
+++ src/main/java/com/euler/solutions/Problem5.java
@@ -2,6 +2,30 @@ package com.euler.solutions;
 
 public class Problem5 {
 	public int calculateSmallestNumberEvenlyDivisibleByAllNumbersBetween(int lowerBoundary, int upperBoundary) {
-		return 2520;
+		int smallestNumber = upperBoundary;
+		boolean tryNextNumber = true;
+
+		while (tryNextNumber) {
+			for (int divisor = lowerBoundary; divisor <= upperBoundary; divisor++) {
+				if (isEvenlyDivisible(smallestNumber, divisor) && divisor == upperBoundary) {
+					tryNextNumber = false;
+				}
+
+				if (isEvenlyDivisible(smallestNumber, divisor) && divisor < upperBoundary) {
+					continue;
+				}
+
+				if (!isEvenlyDivisible(smallestNumber, divisor)) {
+					smallestNumber++;
+					break;
+				}
+			}
+		}
+
+		return smallestNumber;
+	}
+
+	private boolean isEvenlyDivisible(int number, int divisor) {
+		return number % divisor == 0;
 	}
 }
