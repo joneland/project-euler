d2a020e465115b74a3e0afa397f190eb875bde86
Jon Eland <jonathan.eland@gmail.com>
2014-02-08 12:34:50 +0000

    Adding solution for Problem 6

--- src/main/java/com/euler/solutions/Problem6.java
+++ src/main/java/com/euler/solutions/Problem6.java
@@ -1,5 +1,8 @@
 package com.euler.solutions;
 
+import static com.google.common.math.IntMath.checkedAdd;
+import static com.google.common.math.IntMath.checkedMultiply;
+
 public class Problem6 {
 	public int calculateSumSquareDifferenceOfOneTo(int upperBoundary) {
 		int squareOfSums = calculateSquareOfSumsUpTo(upperBoundary);
@@ -9,10 +12,22 @@ public class Problem6 {
 	}
 
 	private int calculateSumOfSquareUpTo(int upperBoundary) {
-		return 385;
+		int total = 0;
+
+		for (int i = 1; i <= upperBoundary; i++) {
+			total = checkedAdd(total, checkedMultiply(i, i));
+		}
+
+		return total;
 	}
 
 	private int calculateSquareOfSumsUpTo(int upperBoundary) {
-		return 3025;
+		int total = 0;
+
+		for(int i = 1; i <= upperBoundary; i++) {
+			total = checkedAdd(total, i);
+		}
+
+		return checkedMultiply(total, total);
 	}
 }
